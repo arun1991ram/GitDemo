dbfd9b178113a973db39f7a84da89908ea4dc88d
arunakumaras <arun1991ram@gmail.com>
2020-01-17 14:24:17 +0530

    this is from other user commit

---
+++ BaseClassLog.py
@@ -0,0 +1,15 @@
+import inspect
+
+import logging
+
+
+class BaseClassLog:
+    def getLogger(self):
+        loggername = inspect.stack()[1][3]
+        logger = logging.getLogger(loggername)
+        fileHandler = logging.FileHandler('logfile.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s :%(name)s :%(message)s")
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)
+        logger.setLevel(logging.DEBUG)
+        return logger
